8a8cbf455f621d7f85738316a2de4b09ed0370b4
GoonWolf <98190554+goonwolf666@users.noreply.github.com>
2026-06-10 11:14:59 +1000

    Add files via upload

---
+++ LoadingScreensMod/config.cpp
@@ -0,0 +1,33 @@
+class CfgPatches
+{
+	class LoadingScreensMod
+	{
+		units[]={};
+		weapons[]={};
+		requiredVersion=1;
+		requiredAddons[]={"DZ_Data"};
+	};
+};
+
+class CfgMods
+{
+    class MyLoadingScreenMod
+    {
+        dir = "LoadingScreensMod";
+        type = "mod";
+        dependencies[] = {"Game"};
+        class defs
+        {
+            class imageSets
+            {
+                files[] = {"LoadingScreensMod/data/loading_screens1.imageset","LoadingScreensMod/data/loading_screens2.imageset"};               
+			};
+            class gameScriptModule
+            {
+                value = "";
+                files[] = {"LoadingScreensMod/scripts/3_game"};
+				
+			};
+		};
+	};
+};
