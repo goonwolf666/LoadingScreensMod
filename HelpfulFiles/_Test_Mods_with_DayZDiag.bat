b25bf68e629914f91241feb50d65313bc30fed91
GoonWolf <98190554+goonwolf666@users.noreply.github.com>
2026-06-21 15:28:54 +1000

    Revise DayZ mod testing batch file
    Updated batch file for testing DayZ mods with new paths and instructions.

--- HelpfulFiles/_Test_Mods_with_DayZDiag.bat
+++ HelpfulFiles/_Test_Mods_with_DayZDiag.bat
@@ -1,41 +1,9 @@
-::	--------	SIMPLE BATCH FILE TO TEST LOCAL DAYZ MODS	--------
-::	* Change paths if your DayZ client, server or 7zip apps are installed in non-default locations
-@ECHO OFF
-CLS
-ECHO(& ECHO(
+:: Simple script to test DayZ mods - requires a simple P:\singleplayer.ChernarusPlus mission folder
+:: See this guide to set that up: https://medium.com/@jkamienski/beginners-guide-to-dayz-mods-development-de3055a10d31
+:: My own set up for that is at: https://github.com/goonwolf666/LoadingScreensMod/tree/main/HelpfulFiles/P_singleplayer.ChernarusPlus
 
-:: 	--------	MOD(S) SETUP	--------
-::	* Keep 'SET "BASEMODLIST=-mod="' as a minimum, or include optional CF/COT/DF if you need admin tools
-REM SET "BASEMODLIST=-mod=C:\Program Files (x86)\Steam\steamapps\common\DayZ\!Workshop\@CF;C:\Program Files (x86)\Steam\steamapps\common\DayZ\!Workshop\@Community-Online-Tools;C:\Program Files (x86)\Steam\steamapps\common\DayZ\!Workshop\@Dabs Framework;"
-SET "BASEMODLIST=-mod="
-SET "TESTMOD=P:\MyScreensRightHere;"
-
-::	--------	CONFIG SETUP	--------
-::	* First option points to vanilla cherno cfg, profile and mission files (needs DayZServer installed)
-::	* Second option points to leaner setup (needs P:\singleplayer.ChernarusPlus setup)
-SET CONFIG="-config=C:\Program Files (x86)\Steam\steamapps\common\DayZServer\serverDZ.cfg"
-SET PROFILE="-profiles=C:\Program Files (x86)\Steam\steamapps\common\DayZServer\ServerProfile"
-SET MISSION="-mission=C:\Program Files (x86)\Steam\steamapps\common\DayZServer\mpmissions\dayzOffline.chernarusplus"
-:: OR use this, comment out the one you dont like
-SET CONFIG="-config=P:\singleplayer.ChernarusPlus\serverDZ.cfg"
-SET PROFILE="-profiles=P:\singleplayer.ChernarusPlus\ServerProfile"
-SET MISSION="-mission=P:\singleplayer.ChernarusPlus"
-
-:: 	--------	BACKUP (optional)	--------
-::	* Having learned the HARD WAY, we back up our packed mod folder every single run (needs 7zip installed)
-MKDIR P:\_baks 2>NUL
-SET "TS=%DATE:~-2%%DATE:~4,2%%DATE:~7,2%%TIME:~0,2%%TIME:~3,2%"
-"C:\Program Files\7-Zip\7z.exe" a "P:\_baks\%TS%.7z" "%TESTMOD:;=%\*" >NUL
-
-::	--------	RUN TIME	--------
-::	* Display summary, go to default client folder and run DayZDiag with above params
-ECHO. MODS: "%BASEMODLIST%%TESTMOD%"
-ECHO. CONFIG: %CONFIG%
-ECHO. PROFILE: %PROFILE% 
-ECHO. MISSION: %MISSION%
-ECHO. BACKUP: "P:\_baks\%TS%.7z"
 C:
 CD "C:\Program Files (x86)\Steam\steamapps\common\DayZ"
-.\DayZDiag_x64.exe %PROFILE% %CONFIG% "%BASEMODLIST%%TESTMOD%" %MISSION% -filePatching -window 
-ECHO(& ECHO(
-PAUSE
+
+:: Update the "-mod=" path below to the folder where you packed your addon:
+.\DayZDiag_x64.exe -mod=P:\LoadingScreensPacked -mission=P:\singleplayer.ChernarusPlus
